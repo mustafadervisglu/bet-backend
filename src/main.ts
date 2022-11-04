15eb682a49953578e228218b459a77e928a68fe8
mustafa <mustafadervisoglu16@gmail.com>
2022-11-04 23:41:35 +0300

    Automated with command MODE=LISTEN_EVENTS

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { BetService } from "src/bet/bet.service";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    const mode = process.env.MODE;
+
+    switch (mode) {
+        case "LISTEN_EVENTS": {
+            const service = app.get(BetService);
+            console.log(service.fetchEventContract());
+            break;
+        }
+        default: {
+            await app.listen(3000);
+        }
+    }
 }
 
 bootstrap();
