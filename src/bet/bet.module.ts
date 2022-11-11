eb7147a8537160b84d2c56a791c896c928ad3b56
mustafa <mustafadervisoglu16@gmail.com>
2022-11-11 15:22:57 +0300

    Imported TypeOrm module with BetEntity

--- src/bet/bet.module.ts
+++ src/bet/bet.module.ts
@@ -1,9 +1,11 @@
-import { Global, Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { BetService } from "src/bet/bet.service";
 import { BetController } from "src/bet/bet.controller";
+import { BetEntity } from "src/bet/bet-entity/bet.entity";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
-@Global()
 @Module({
+    imports: [TypeOrmModule.forFeature([BetEntity])],
     controllers: [BetController],
     providers: [BetService],
 })
