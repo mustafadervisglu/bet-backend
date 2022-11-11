3f525edbc840c4f3fcae221d69951a0afab04a33
mustafa <mustafadervisoglu16@gmail.com>
2022-11-11 15:27:07 +0300

    Injected BetEntity for Repository, Configrated .env file as configService and Added allEvent method for listening events from contract

--- src/bet/bet.service.ts
+++ src/bet/bet.service.ts
@@ -1,15 +1,21 @@
-import { Injectable, Logger } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import Web3 from "web3";
 import betAbi from "./abi/bet-abi.json";
 import { AbiItem } from "web3-utils";
 import { ConfigService } from "@nestjs/config";
+import { InjectRepository } from "@nestjs/typeorm";
+import { BetEntity } from "src/bet/bet-entity/bet.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class BetService {
-    private readonly events = [];
     private web3: Web3;
 
-    constructor(private configService: ConfigService) {
+    constructor(
+        @InjectRepository(BetEntity)
+        private betRepository: Repository<BetEntity>,
+        private configService: ConfigService
+    ) {
         this.web3 = new Web3(
             new Web3.providers.WebsocketProvider(
                 this.configService.get<string>("GOERLI")
@@ -18,24 +24,27 @@ export class BetService {
     }
 
     async fetchEventContract() {
-        const contractAddress = "0x1B09FB700630156Bf242DdAe6944300C48E90701";
+        const contractAddress =
+            this.configService.get<string>("CONTRACT_ADDRESS");
         const myContract = new this.web3.eth.Contract(
             betAbi as AbiItem[],
             contractAddress
         );
 
-        const events = myContract.getPastEvents(
-            "Lose",
-            {
-                fromBlock: 0,
-                toBlock: "latest",
-            },
-            (error, events) => {
-                if (error) {
-                    console.log(error);
-                }
-                console.log(events[0].returnValues);
-            }
-        );
+        const events = myContract.events
+            .allEvents({})
+            .on("data", async (events) => {
+                const eventValue = events.returnValues;
+                const games = {
+                    id: eventValue.id,
+                    bet: eventValue.bet,
+                    randomNumber: eventValue.randomNumber,
+                    amount: eventValue.amount,
+                    player: eventValue.player,
+                    time: eventValue.time,
+                };
+                console.log(games);
+                await this.betRepository.save(games);
+            });
     }
 }
