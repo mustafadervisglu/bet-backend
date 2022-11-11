a74ca914f65be43424f8cb313e8ae03bafe596c4
mustafa <mustafadervisoglu16@gmail.com>
2022-11-11 15:29:58 +0300

    Added useGlobalPipes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { BetService } from "src/bet/bet.service";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-
+    app.useGlobalPipes(
+        new ValidationPipe({ whitelist: true, transform: true })
+    );
     const mode = process.env.MODE;
 
     switch (mode) {
